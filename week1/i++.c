2771601abfa1a852bb0f8fb9c2c850c48a5f94e7
eoieiie <turkiyecivciv31@gmail.com>
2023-09-19 10:48:18 +0900

    difference explained

---
+++ week1/i++.c
@@ -0,0 +1,34 @@
+/* some powers of 2 are printed */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0, power = 1;
+
+    while(i++ <= 10) // 변수를 사용한 후 값을 증가(조건 검사 <=10 한 다음 i를 증가시킴)
+    printf("%-6d", power *= 2);
+    printf("\n");
+
+    return 0;
+}
+
+/* 
+++i
+1이네**
+2이네
+3이네
+.
+.
+.
+10이네
+
+i++
+0이네 **
+1이네
+2이네
+.
+.
+.
+10이네
+ */
