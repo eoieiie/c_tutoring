e1cbfc7437893475acfbfa6428021960a53e2e23
eoieiie <turkiyecivciv31@gmail.com>
2023-10-12 23:28:21 +0900

    new file added

---
+++ week2/test.c
@@ -0,0 +1,16 @@
+/* 
+6가지 토큰 존재. 
+
+keyword : int, float, if, while 등 이미 c언 상에서 지정되어 있는 것.
+
+identifiers : 객체에 이름을 줌. 변수나 함수 이름을 만드는 것. 내가 이름을 짓는 것
+
+constants : 정수, 실수로 구분되며 다양한 종류의 값을 나타냄. 
+
+string constant : " 뭐시기 "
+
+operators : 
+
+punctuators :  
+*/
+
